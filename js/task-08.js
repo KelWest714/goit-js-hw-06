171a54c3b740cbdef5dfbf0a00c5c56372b7d4d2
Volodymyr <104516153+KelWest714@users.noreply.github.com>
2022-09-09 21:18:18 +0300

    task-com

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formFieldEl = document.querySelector('.login-form');
 
+formFieldEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const email = e.currentTarget.elements.email.value;
+  const password = e.currentTarget.elements.password.value;
+
+  if (!email || !password) {
+    window.alert('Please, fill in all fields!');
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  if (email && password) {
+    e.currentTarget.reset();
+  }
+}
